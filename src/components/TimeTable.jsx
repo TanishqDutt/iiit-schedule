8a3cc14eb14ee4b9cecaca6af884fc7656a83c94
Tanishq Dutt Mathur <tanishqduttmathur@gmail.com>
2026-08-24 02:17:05 +0530

    Added a break segment

--- src/components/TimeTable.jsx
+++ src/components/TimeTable.jsx
@@ -15,20 +15,39 @@ const TimeTableBlock = () => {
   return (
     <div className="timetableblock">
 
-      {days.map((elem) => <div className="row" key={elem}>
+      {days.map((elem) => 
+      <div className="row" key={elem}>
 
-        {sem3.sem3.filter((c) => c.day == elem).map((cls) => {
-          
-          return(
-            <div className="class" style={{
-              left:(cls.start*120).toString()+"px",
-              width:((cls.end - cls.start + 1)*120).toString()+"px"
-            }}>
-              {cls.subjectcode}
-            </div>
-          )
+        <div className="section before">
+          {sem3.sem3.filter((c) => c.day == elem && c.end<=3).map((cls) => {
+            
+            return(
+              <div className="class" style={{
+                left:(cls.start*120).toString()+"px",
+                width:((cls.end - cls.start + 1)*120).toString()+"px"
+              }}>
+                {cls.subjectcode}
+              </div>
+            )
 
-        })}
+          })}
+        </div>
+        <div className="break"></div>
+        <div className="section after">
+          {sem3.sem3.filter((c) => c.day == elem && c.start>=4).map((cls) => {
+            
+            return(
+              <div className="class" style={{
+                left:((Number(cls.start) - 4)*120).toString()+"px",
+                width:((cls.end - cls.start + 1)*120).toString()+"px"
+              }}>
+                {cls.subjectcode}
+              </div>
+            )
+
+          })}
+        </div>
+
 
       </div>)}
 
